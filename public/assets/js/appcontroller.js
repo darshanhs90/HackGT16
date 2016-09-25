9c858298eddc7d8dc7debd959fc6a3c5a0fb8039
Nagabharan N <nnagabharan@gmail.com>
2016-09-25 02:12:26 -0400

    ui changes

--- public/assets/js/appcontroller.js
+++ public/assets/js/appcontroller.js
@@ -1,8 +1,9 @@
 var app=angular.module('myApp',[]);
 app.controller('myCtrl',function($scope,$http) {
+	$scope.dest="";
     $scope.getPlace = function(){
         console.log($scope.dest);
     }
     
-
+		
 });
